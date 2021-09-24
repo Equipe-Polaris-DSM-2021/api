4194e7de71be66bc6537c667cac459d9b6af4576
Gabriel Camargo <gabriel.cleite@outlook.com>
2021-09-23 22:04:51 -0300

    testando viabilidade do esquema de pastas

---
+++ src/app/controllers/satellites/Landsat8Controller.ts
@@ -0,0 +1,43 @@
+import { Request, Response } from 'express';
+const dfd = require("danfojs-node")
+const tf = require("@tensorflow/tfjs-node")
+
+class Landsat8Controller {
+  async index(req: Request, res: Response) {
+    const Landsat4_8_csv_URL: string = 'https://storage.googleapis.com/gcp-public-data-landsat/index.csv.gz'
+    dfd.read_csv(
+        Landsat4_8_csv_URL, {
+        hasHeader: false,
+        delimiter: ',',
+        columnNames: [
+          'sensor', 'path', 'row', 'acquisition_date', 'processing_level',
+          'ul_lat', 'ul_lon', 'ur_lat', 'ur_lon',
+          'lr_lat', 'lr_lon', 'll_lat', 'll_lon',
+          'download_url',
+          'band_5_gain', 'band_6_gain', 'band_7_gain', 'band_8_gain',
+          'sun_elevation', 'sun_azimuth',
+          'band_5_download_url', 'band_6_download_url', 'band_7_download_url', 'band_8_download_url'
+        ]
+      }
+      ).then((df: any) => {
+    
+        df.query({
+          column: "path", is: "==", to: "151", inplace: true
+        })
+    
+        df.query({
+          column: "row", is: "==", to: "102", inplace: true
+        })
+    
+        df.to_json().then((json: any) => {
+          return res.send(JSON.parse(json))
+        })
+    
+      }).catch((err: any) => {
+        console.log("=====================\nerror: " + err + "\n=====================")
+        return res.json({"message": err})
+      })
+  }
+}
+
+export default new Landsat8Controller();
--- src/routes.ts
+++ src/routes.ts
@@ -1,9 +1,18 @@
 import { Router, Response, response } from 'express';
 import UserController from './app/controllers/UserController';
 
+import Landsat8Controller from './app/controllers/satellites/Landsat8Controller';
+import Sentinel2Controller from './app/controllers/satellites/Sentinel2Controller';
+import Cbers4Controller from './app/controllers/satellites/Cbers4Controller';
+
+
 const router = Router();
 
 router.get('/users', UserController.index)
 router.post('/users', UserController.store);
+
+router.get('/landsat8', Landsat8Controller.index)
+router.get('/sentinel2', Sentinel2Controller.index)
+router.get('/cbers4', Cbers4Controller.index)
 
 export default router;
