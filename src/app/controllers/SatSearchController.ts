7c86f54971b80b6929ff23ed467b24ea21b95748
Gabriel Camargo <gabriel.cleite@outlook.com>
2021-09-29 22:51:33 -0300

    requisitando informaçoes do body da requisiçao

--- src/app/controllers/SatSearchController.ts
+++ src/app/controllers/SatSearchController.ts
@@ -3,40 +3,36 @@ import fetch from 'node-fetch';
 
 class SatSearchController {
   async index(request:Request, response:Response) {
+    const {bbox, cloudCover, time} = request.body
+    
     const inputBody = `{
-      "bbox": [
-        -45.8870659,
-        23.1702737,
-        -45.8921662,
-        -23.1713824
-      ],
-      "time": "2018-02-12T00:00:00Z/2018-03-18T12:31:12Z",
+      "bbox": ${bbox},
+      "time": ${time},
       "intersects": null,
       "query": {
         "eo:cloud_cover": {
-          "lt": 50
+          "lt": ${cloudCover}
         }
       },
       "sort": [
         {
           "field": "eo:cloud_cover",
-          "direction": "desc"
+          "direction": "asc"
         }
       ]
     }`;
+
     const headers = {
       'Content-Type':'application/json',
       'Accept':'application/geo+json'
     
     };
     
-    fetch('https://earth-search.aws.element84.com/v0/search',
-    {
+    fetch('https://earth-search.aws.element84.com/v0/search', {
       method: 'POST',
       body: inputBody,
       headers: headers
-    })
-    .then(function(res) {
+    }).then(function(res) {
         return res.json();
     }).then(function(body) {
       response.json(body);
