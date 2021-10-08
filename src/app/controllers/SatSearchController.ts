e0052f912836812382ab14b71a066e6c6cffda3c
Gabriel Camargo <gabriel.cleite@outlook.com>
2021-10-08 10:33:42 -0300

    adds bbox method query in mongodb

--- src/app/controllers/SatSearchController.ts
+++ src/app/controllers/SatSearchController.ts
@@ -6,18 +6,27 @@ class SatSearchController {
   async index(request: Request, response: Response) {
     try {
       const { bbox, cloudCover, date_initial, date_final } = request.body
-      
+
       const imagesLandsat = await Landsat.find({
         'properties.eo:cloud_cover': {
-            '$lte': cloudCover
+          '$lte': cloudCover
         },
         'properties.datetime': {
-            '$gte': date_initial, 
-            '$lte': date_final
+          '$gte': date_initial,
+          '$lte': date_final
         }
-    })
-      
-      return response.json({"features": imagesLandsat})
+      })
+
+      const features = await imagesLandsat.filter((doc:any) => {
+        if (
+          doc.bbox[0] <= bbox[0] && doc.bbox[2] >= bbox[0] || 
+          doc.bbox[1] <= bbox[1] && doc.bbox[3] >= bbox[1]
+        ){
+          return doc
+        }
+      })
+
+      return response.json({ "features": features })
 
     } catch (err) {
       // console.log(err)
